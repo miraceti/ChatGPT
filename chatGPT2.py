892b2c16a941963c8a0d96d845297ca52461b6b6
miraceti <elecam@gmail.com>
2023-02-05 15:59:09 +0100

    vox1

---
+++ chatGPT2.py
@@ -0,0 +1,29 @@
+import openai
+import speech_recognition as sr
+import pyttsx3
+
+# Configurer une clé d'API valide
+openai.api_key = "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"
+
+# Initialiser le moteur de synthèse vocale
+engine = pyttsx3.init()
+
+while True:
+    # Demander à l'utilisateur de saisir une recherche
+    prompt = input("Tape ta question ('exit' pour sortir) : ")
+    if prompt == "exit":
+        break
+    # Envoyer une requête à l'API de GPT
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=2048,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    # Afficher la réponse gt1
+    print(response["choices"][0]["text"])
+    engine.say(response["choices"][0]["text"])
+    engine.runAndWait()
