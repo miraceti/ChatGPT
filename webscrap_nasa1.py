8667e07ae89ef12eefed4c2593fba6138264f163
miraceti <elecam@gmail.com>
2023-02-12 16:55:59 +0100

    calculmath1

--- webscrap_nasa1.py
+++ webscrap_nasa1.py
@@ -1,11 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.nasa.gov/mission_pages/station/expeditions/index.html"
+url = "https://www.nasa.gov/mission_pages/juno/main/index.html"
 page = requests.get(url)
 html_content = page.content
 soup = BeautifulSoup(html_content, "html.parser")
-
+print(soup)
 #title = soup.find("h1").text
 #print(title)
 
@@ -15,13 +15,14 @@ soup = BeautifulSoup(html_content, "html.parser")
 # else:
 #     title = "Titre non trouvé"
 
-# links = soup.find_all("a")
-# for link in links:
-#     print(link.text, link["href"])
+links = soup.find_all("link")
+print(links)
+for link in links:
+    print(link.text, link["href"])
 
-link_elements = soup.find_all("a")
-if link_elements:
-    for link in link_elements:
-        print(link.text, link["href"])
-else:
-    print("Aucun lien trouvé")
+# link_elements = soup.find_all("a")
+# if link_elements:
+#     for link in link_elements:
+#         print(link.text, link["href"])
+# else:
+#     print("Aucun lien trouvé")
